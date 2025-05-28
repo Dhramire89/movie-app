3b89021c3658dcf471de44ceb1641ba53403da84
dramirez <dramirezrrzz@gmail.com>
2025-05-27 20:41:01 -0600

    Instancia de Axios creada

---
+++ core/actions/movies/now-playing.action.ts
@@ -0,0 +1,8 @@
+export const nowPlayingAction = async () => {
+  try {
+  } catch (error) {
+    console.error(error);
+    throw "Error al cargar las peliculas actuales:"; // Re-throw the error for further handling if needed
+  }
+  return {};
+};
