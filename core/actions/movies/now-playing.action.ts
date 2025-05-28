6feb030ddcbd0cfd80369e06156a81b0958ca4da
dramirez <dramirezrrzz@gmail.com>
2025-05-27 21:31:38 -0600

    Danto tipado a axios

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,9 +1,11 @@
+import { MovieDBMoviesResponse } from "@/infrastructure/interfaces/moviedb-response";
 import { movieApi } from "../api/movie-api";
 
 export const nowPlayingAction = async () => {
   try {
-    const { data } = await movieApi.get("/now_playing");
+    const { data } = await movieApi.get<MovieDBMoviesResponse>("/now_playing");
     console.log("Peliculas actuales:", JSON.stringify(data));
+
     return [];
   } catch (error) {
     console.error(error);
