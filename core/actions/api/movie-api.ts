39af5d1ba4d1f88a2e228d17b27aae3b38a9c996
dramirez <dramirezrrzz@gmail.com>
2025-06-09 20:45:05 -0600

    Mappers funcionando

--- core/actions/api/movie-api.ts
+++ core/actions/api/movie-api.ts
@@ -4,8 +4,10 @@ import axios from "axios";
 //se crea una instancia de axios con la URL base de la API de películas
 // y se exporta para que pueda ser utilizada en otros archivos
 export const movieApi = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_MOVIE_URL,
+  baseURL: process.env.EXPO_PUBLIC_MOVIE_URL, // para tener acceso a la URL base de la API de películas
+  // se utiliza process.env.EXPO_PUBLIC_MOVIE_URL para obtener la URL base de la API de películas
   params: {
+    // parámetros por defecto para las peticiones a la API
     language: "es-ES", // idioma por defecto costa rica
     api_key: process.env.EXPO_PUBLIC_MOVIE_API_KEY, // clave de la API
   },
--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,12 +1,15 @@
+import { MovieMapper } from "@/infrastructure/interfaces/mappers/movie.mapper";
 import { MovieDBMoviesResponse } from "@/infrastructure/interfaces/moviedb-response";
 import { movieApi } from "../api/movie-api";
 
 export const nowPlayingAction = async () => {
   try {
     const { data } = await movieApi.get<MovieDBMoviesResponse>("/now_playing");
-    console.log("Peliculas actuales:", JSON.stringify(data));
+    //console.log("Peliculas actuales:", JSON.stringify(data, null, 2));// data es esl objeto a imprimir, null es el espacio de sangrado y 2 es el número de espacios de sangrado
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
 
-    return [];
+    console.log("Peliculas actuales:", JSON.stringify(movies, null, 2)); // Imprime las peliculas actuales en consola
+    return movies;
   } catch (error) {
     console.error(error);
     throw "Error al cargar las peliculas actuales:"; // Re-throw the error for further handling if needed
